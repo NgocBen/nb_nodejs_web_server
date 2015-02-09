cd3215c18c9eff623decaa9ffb9a2249f55ef6cf
NgocBen <ngocben@live.com>
2015-02-10 00:19:14 +0700

    delete

--- ser.js
+++
@@ -1,6 +0,0 @@
-server = require('http').createServer(function (req, res) {
-    res.writeHead(200, {'content-type':'text/html'});
-    res.write('NgocBen');
-    res.end();
-});
-server.listen(8080);
